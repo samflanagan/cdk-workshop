4b97871dfcc63ae420aa65d02f887c594663c9e8
Sam Flanagan <156433047+samflanagan@users.noreply.github.com>
2024-03-01 16:33:41 +0000

    .

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,19 +2,21 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {WorkshopPipelineStage} from './pipeline-stage';
 import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
-import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 
 export class WorkshopPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
         
-        // Lookup GitHub connection ARN from SSM Parameter Store
-        const githubConnectionArn = ssm.StringParameter.valueFromLookup(this, 'github-samflanagan-connection-arn');
+        // Creates a CodeCommit repository called 'WorkshopRepo'
+        new codecommit.Repository(this, 'CdkWorkshopRepo', {
+        repositoryName: "CdkWorkshopRepo"
+        });
 
         // Pipeline code goes here
         // The basic pipeline declaration. This sets the initial structure
         // of our pipeline
-       const pipeline = new CodePipeline(this, 'Pipeline', {
+  /*      const pipeline = new CodePipeline(this, 'Pipeline', {
         pipelineName: 'WorkshopPipeline',
         synth: new CodeBuildStep('SynthStep', {
                 input: CodePipelineSource.connection('samflanagan/cdk-workshop', 'master', { connectionArn: githubConnectionArn}),
@@ -31,7 +33,7 @@ export class WorkshopPipelineStack extends Stack {
     });
     
     const deploy = new WorkshopPipelineStage(this, 'Deploy');
-    const deployStage = pipeline.addStage(deploy);
+    const deployStage = pipeline.addStage(deploy); */
 
     }
 }
